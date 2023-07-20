ca2540ff54abf116d47275e3b1b2f4b24af92eec
mss-souloftware <sufyan.souloftware@gmail.com>
2023-07-20 17:54:01 +0500

    hero home

---
+++ components/Home/HeroSection/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Container from '../../Container/Container'
+
+export default function HomeHero() {
+    return (
+        <section id="homeHero">
+            <Container>
+
+            </Container>
+        </section>
+    )
+}
